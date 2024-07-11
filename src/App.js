6eda956fa65ca5c4dc6deda38da5584fdaa27a63
Kelly Do <kellydo8467@gmail.com>
2024-07-11 12:38:40 -0400

    initialized react app

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className='highlighted'>
+          Hi there,
+        </p>
+        <h1>My name is Kelly Do</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

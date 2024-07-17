664fd176480f37a2a5ca8d8a13a427a69d6db060
Kelly Do <kellydo8467@gmail.com>
2024-07-17 13:24:30 -0400

    refactored carousel so information changes according to the current page they are on. also added fuctionality and transition to the image toggle

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,33 +1,97 @@
 import '../styles/Projects.css';
-import budgetAdd from '../imgs/add.png';
-import dashboard from '../imgs/dashboard.png';
+import BudgetBuddy1 from '../imgs/BudgetBuddy1.png';
+import BudgetBuddy2 from '../imgs/BudgetBuddy2.png';
+import {projectNames, skills, bulletPoints} from '../info/project-info.js';
+import { useEffect, useState } from 'react';
 
+console.log(projectNames);
 const Projects = () => {
+    const [currentBtn, setCurrentBtn] = useState('img-btn1')
+    const [currentPg, setCurrentPg] = useState(projectNames[0])
+    const [currentIndex, setCurrentIndex] = useState(0);
+
     return (
         <div className="element">
             <h1>Projects</h1>
-            <Carousel />
+            <Carousel currentBtn = {currentBtn} setCurrentBtn={setCurrentBtn} currentPg = {currentPg} setCurrentPg = {setCurrentPg} currentIndex={currentIndex} setCurrentIndex={setCurrentIndex}/>
         </div>
     )
 }
 
-
-const Carousel = () => {
+const Skills = ({ skills }) => {
     return (
-        <div className = 'container'>
+        <div className='container'>
+            {skills.map((skill, index) => (
+                <div key={index} className='skill-bubble'>
+                    {skill}
+                </div>
+            ))}
+        </div>
+    );
+};
+
+
+const Carousel = ({currentBtn, setCurrentBtn, currentPg, setCurrentPg, currentIndex, setCurrentIndex}) => {
+    const nextPage = (event) => {
+        event.preventDefault();
+        let newIndex;
+        if (currentIndex !== projectNames.length - 1) {
+            newIndex = currentIndex + 1;
+        } else {
+            newIndex = 0;
+        }
+        setCurrentIndex(newIndex);
+        setCurrentPg(projectNames[newIndex]);
+    };
+
+    const backPage = (event) => {
+        event.preventDefault();
+        let newIndex;
+        if (currentIndex !== 0) {
+            newIndex = currentIndex - 1;
+        } else {
+            newIndex = projectNames.length - 1;
+        }
+        setCurrentIndex(newIndex);
+        setCurrentPg(projectNames[newIndex]);
+    };
+
+    const changeImg = (event) => {
+        const btn = event.target.id;
+        setCurrentBtn(btn);
+    }
+
+    return (
+        <div className = 'container' style={{alignItems: 'center'}}>
+            <button id='triangle-btn-left' onClick={backPage}>;</button>
             <div id = 'img-carousel'>
                 <div className='showcase'>
-                    <img src={budgetAdd} className='demo-img'></img>
+                    <div style={{width: '42vw'}}>
+                        <img src={BudgetBuddy1} className={`demo-img ${currentBtn === 'img-btn1' ? 'active' : ''}`} id='img-1' alt=''></img>
+                        <img src={BudgetBuddy2} className={`demo-img ${currentBtn === 'img-btn2' ? 'active' : ''}`} id='img-2' alt=''></img>
+                    </div>
                 </div>
+
                 <div id = 'carousel-btns'>
-                    <button className='dot-btn' id = 'img-btn1'>.</button>
-                    <button className='dot-btn'>.</button>
+                    <button className={`dot-btn ${currentBtn == 'img-btn1' ? 'active' : ''}`} id = 'img-btn1' onClick={changeImg}>.</button>
+                    <button className={`dot-btn ${currentBtn == 'img-btn2' ? 'active' : ''}`} id = 'img-btn2' onClick={changeImg}>.</button>                
                 </div>
             </div>
-            <div>
-                <p>this is text</p>
+            <div className='project-info'>
+                <h1 className='position-title' style={{marginLeft : '35px'}}>{currentPg}</h1>
+                <ul className='bullets'>
+                   {bulletPoints[currentPg].map((point, index) =>(
+                    <li className='points'>{point}</li>
+                   ))}
+                </ul>
+                <Skills skills={skills[currentPg]}/>
             </div>
+            <button id='triangle-btn-right' onClick={nextPage}> </button>
+
         </div>
     )
+
+
+
 }
 export default Projects;
---
+++ src/info/project-info.js
@@ -0,0 +1,25 @@
+export const projectNames = ['Budget Buddy', 'Best Professor', 'SpotiU']
+export const skills = {
+    'Budget Buddy' : ['SQL', 'Python', 'Flask', 'Chart.py', 'React'],
+    'Best Professor' : ['GraphQL', 'JavaScript', 'Selenium', 'Spring-boot', 'Java'],
+    'SpotiU' : ['Python', 'JavaScript', 'Spotipy']
+};
+
+export const bulletPoints = {
+    'Budget Buddy' : [
+        'Initialized MySQL databases with a hierarchy (find out)', 
+        'Utilized React hooks and states for an interactive and diverse experience while adding budget categories and expenses',
+        'Hooked a CRUD Flask server that handled user registration, expense tracking, and budget summaries'
+    ],
+    'Best Professor' : [
+        'Allowed users to input their school and courses and generated the top 5 professors instructing each course',
+        'Called fetch queries to ratemyprofessor.com’s graphQL to display options of professors, schools, and courses',
+        'Created JavaScript functions that filtered through ratings to efficiently output the best professors within seconds',
+        'Established a system of React components and states to limit GraphQL calls, allowing information to display quicker'
+    ],
+    'SpotiU' : [
+        'Develop Python scripts using Spotify’s Web API to optimize Spotify’s song recommendations for the user',
+        'Utilize Spotipy lib to process JSON data (artists, tracks, and playlists) to create filters for Spotify-generated playlists',
+        'Implement user authorization with Oauth 2.0 framework for secure access about listening history and preferences'
+    ]
+}

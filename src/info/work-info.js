885dfae80462af27957f6f82aba47c21e42091f8
Kelly Do <kellydo8467@gmail.com>
2024-08-20 11:54:20 -0400

    added more points to Aramark position

--- src/info/work-info.js
+++ src/info/work-info.js
@@ -1,9 +1,11 @@
 export const workplaces = ['Aramark']
 export const workBulletPoints = {
     'Aramark' : [
-        'Automated chargeback calculations from large, monthly Azure billing CSVs, saving 90% time and effort allocated',
-        'Utilized Pandas and Openpyxl to synthesis data from CSVs to create sheets detailing taxes and charges by department',
-        'Resolved Azure resources and AD permissions requests/issues from ServiceNow, enhancing project productivity'
+        'Automated chargeback calculations from dense, monthly Azure billing CSVs, using Pandas and Openpyxl to synthesis data and create sheets detailing department charges and taxes, saving 90% time and effort allocated',
+        'Detailed project design documents for robotic process automations, resulting $10,000+ in cost savings annually',
+        'Administered design documents to create an automated solution on UiPath, streamlining repetitive human tasks, such as document extraction and uploading, enhancing operational efficiency',
+        "Developed Python functions using SWQL to retrieve subnets from respective IP addresses and identify duplicate networks, efficiently progressing the enforcement of least privilege access across 1000+ users",
+        "Resolved Azure resources and AD permissions requests/issues from ServiceNow, advancing project development"
     ]
 }
 

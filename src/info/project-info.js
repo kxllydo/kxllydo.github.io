a554f0dfb2cec55b0dea3b889392963f9a148603
Kelly Do <kellydo8467@gmail.com>
2024-08-20 11:13:03 -0400

    updated resume bullet points for personal projects

--- src/info/project-info.js
+++ src/info/project-info.js
@@ -7,15 +7,14 @@ export const skills = {
 
 export const bulletPoints = {
     'Budget Buddy' : [
-        'Initialized MySQL databases with a hierarchy (find out)', 
-        'Utilized React hooks and states for an interactive and diverse experience while adding budget categories and expenses',
-        'Hooked a CRUD Flask server that handled user registration, expense tracking, and budget summaries'
+        'Constructed a Flask API with MySQL integration that handled user authentication, session handling, and CRUD operations such as expense tracking, category creation, and spending limitations', 
+        'Leveraged React hooks and state management to create dynamic and interactive components, enhancing user experience while navigating through expense summaries, spending categories, and budgets',
+        'Applied agile principles and scrum methodologies on a team to effectively outline and collaborate on user-orientated features'
     ],
     'Best Professor' : [
         'Allowed users to input their school and courses and generated the top 5 professors instructing each course',
-        'Called fetch queries to ratemyprofessor.com’s graphQL to display options of professors, schools, and courses',
-        'Created JavaScript functions that filtered through ratings to efficiently output the best professors within seconds',
-        'Established a system of React components and states to limit GraphQL calls, allowing information to display quicker'
+        "Reverse-engineered ratemyprofessor.com’s GraphQL queries to gather and refine data about professors, schools, and courses, improving the accuracy and relevance of the displayed information",
+        "Created JavaScript functions that filtered through 1-100 professors’ ratings to proficiently output the best professors per course within seconds, ensuring that the user has a quick and informational result",
     ],
     'SpotiU' : [
         'Develop Python scripts using Spotify’s Web API to optimize Spotify’s song recommendations for the user',
